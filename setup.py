70d4a1948384d5b145df32c137f090c063ec81f3
cagostino <cjp.agostino@gmail.com>
2026-01-28 16:29:11 -0500

    jinx ui fixings , team ui management , config ui, new kg ui

--- setup.py
+++ setup.py
@@ -112,7 +112,7 @@ def get_package_data_patterns():
 
 setup(
     name="npcsh",
-    version="1.1.19",
+    version="1.1.20",
     packages=find_packages(exclude=["tests*"]),
     install_requires=base_requirements,  # Only install base requirements by default
     extras_require={
