4af6aa9919af7275b88f1098adbca4a8a233be0a
Chris Agostino <cjp.agostino@gmail.com>
2026-01-30 10:50:22 -0500

    git jinx and pyver

--- setup.py
+++ setup.py
@@ -112,7 +112,7 @@ def get_package_data_patterns():
 
 setup(
     name="npcsh",
-    version="1.1.20",
+    version="1.1.21",
     packages=find_packages(exclude=["tests*"]),
     install_requires=base_requirements,  # Only install base requirements by default
     extras_require={
