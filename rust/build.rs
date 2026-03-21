6ea02507730956a9a5cc711f26ca9c1bfd3d494f
Chris Agostino <cjp.agostino@gmail.com>
2026-03-21 17:59:36 -0400

    fix: VERSION file in repo root, setup.py and build.rs read from it

--- rust/build.rs
+++ rust/build.rs
@@ -1,8 +1,8 @@
 fn main() {
-    let version = std::fs::read_to_string("../../VERSION")
+    let version = std::fs::read_to_string("../VERSION")
         .unwrap_or_else(|_| "0.0.0".to_string())
         .trim()
         .to_string();
     println!("cargo:rustc-env=NPCSH_VERSION={}", version);
-    println!("cargo:rerun-if-changed=../../VERSION");
+    println!("cargo:rerun-if-changed=../VERSION");
 }
